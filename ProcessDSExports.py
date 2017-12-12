1b2d5b818b9a66663b0dbc6677c367b0ea093fec
tshynik <lawolberg@gmail.com>
2017-12-12 16:33:02 -0500

    working version of code

--- ProcessDSExports.py
+++ ProcessDSExports.py
@@ -49,97 +49,117 @@ def formatfile(infilename, outfilename):
 		
 		## process header line
 		if linenum == 1:
-			messaging("Header: %s" % line)
+			#messaging("Header: %s" % line)
 			if len(line)==1:
 				messaging("Error: First line (header) is not comma-separated. Check that the first line contains the headers. Also check that this is actually a CSV file.")
 				return("error")
 			header = line
 			newheader = ','.join(map(str, (header[ i ] for i in range(0,len(header))))) + "\n"
 			outfile.write( newheader )
-			continue;
-		
-		## if this isn't a new person (the "office code" field is blank), go to next line.
-		## If it is a new person, count a new person and go on to read the next 7(?) lines as well.
-		#line[0] = line[0].strip('\"\'')
-		if len(line[0])==0 :
 			continue
-		elif len(line[0])!=4:
-			messaging("Warning: On line %s, Office Code may be incorrect: %s" % (linenum, line[0]) )
-		count += 1
+		
+		## If it is a new person:
+		##	write the last person to file (assuming it's not the first person)
+		##	count a new person
+		##	start counting the next lines as belonging to the same person (subline counter)
+		#line[0] = line[0].strip('\"\'')
+		if len(line[0])!=0 :
+			if count!=0:
+				##write our consolidated data to file!
+				linestr = ','.join(map(str, (output[ i ] for i in range(len(header)) ))) + "\n"
+				# http://stackoverflow.com/questions/44778/
+				outfile.write( linestr )
+			##start a new person
+			subline = 0
+			count += 1
+			if len(line[0])!=4:
+				messaging("Warning: On line %s, Office Code may be incorrect: %s" % (linenum, line[0]) )
+			output = line
+		else:
+			subline += 1
 		
 		## tax form fields are on the second line
-		next(infile)
-		next(infile)
-		messaging("Line 2: %s" % line)
-		# line2 = line2.split(",")
-		# line2[-1] = line2[-1].strip('\"\'')
-		# if len(header) != len(line2):
-			# messaging("Error: Different number of columns in record %s, line 2 (file line # %s) than in header." % (count, linenum) )
-			# messaging("Person: %s\nProblematic line: %s" % (line,line2) )
-			# return("error")
-		# line2[-1] = line2[-1].strip()
-		# #get the index # for these columns: futureproofing, in case more cols are added later
-		# tax_headers = ['w4', 'witholding', 'additional amount', 'exempt']
-		# for i in range(len(tax_headers)):
-			# try:
-				# #index = header.index( tax_headers[i] )
-				# #line[index] = line2[index]
-				# #messaging("Step %s, index of %s: %s" % ( i, tax_headers[i] , header.index( tax_headers[i] )) )
-				# line[ header.index( tax_headers[i] ) ] = line2[ header.index( tax_headers[i] ) ]
-			# except ValueError:
-				# if count==1:
-					# messaging("Warning: %s column is missing" % tax_headers[i] )
-					# if warning == 0:
-						# messaging("File Headers: %s" % header)
-						# warning = 1
-			# except IndexError:
-				# messaging("Error on line %s of file. Count: %s, Step: %s, Index of %s: %s" % ( linenum, count, i, tax_headers[i] , header.index( tax_headers[i] )) )
-				# messaging("Person: %s\nProblematic line: %s" % (line,line2) )
-				# return("error")
+		if subline==1:
+			#messaging("Line 2: %s" % line)
+			if len(header) != len(line):
+				messaging("Error: Different number of columns in record %s, line 2 (file line # %s) than in header." % (count, linenum) )
+				messaging("Person: %s\nProblematic line: %s" % (line,line2) )
+				return("error")
+			##get the index # for these columns: futureproofing, in case more cols are added later
+			tax_headers = ['w4', 'witholding', 'additional amount', 'exempt']
+			for i in range(len(tax_headers)):
+				try:
+					#index = header.index( tax_headers[i] )
+					#output[index] = line[index]
+					#messaging("Step %s, index of %s: %s" % ( i, tax_headers[i] , header.index( tax_headers[i] )) )
+					output[ header.index( tax_headers[i] ) ] = line[ header.index( tax_headers[i] ) ]
+				except ValueError:
+					if count==1:
+						messaging("Warning: %s column is missing." % tax_headers[i] )
+						if warning == 0:
+							messaging("Available columns: %s" % header)
+							warning = 1
+				except IndexError:
+					messaging("Error on line %s of file. Count: %s, Step: %s, Index of %s: %s" % ( linenum, count, i, tax_headers[i] , header.index( tax_headers[i] )) )
+					messaging("Person: %s\nProblematic line: %s" % (output,line) )
+					return("error")
 		
-		# # effective date is on the third line
-		# line3 = infile.readline()
-		# line3 = line3.split(",")
-		# if len(header) != len(line2):
-			# messaging("Error: Different number of columns in record %s, line 3 than in header" % count)
-			# return("error")
-		# line3[-1] = line3[-1].strip()
-		# try:
-			# effe_index = header.index("effective date")
-			# line[effe_index] = line3[effe_index]
-		# except ValueError:
-			# if count==1:
-				# messaging("Warning: effective date column is missing" )
-				# if warning == 0:
-					# messaging("File Headers: %s" % header)
-					# warning = 1
+		## effective date is on the third line
+		if subline==2:
+			if len(header) != len(line):
+				messaging("Error: Different number of columns in record %s, line 2 (file line # %s) than in header." % (count, linenum) )
+				messaging("Person: %s\nProblematic line: %s" % (line,line2) )
+				return("error")
+			##get the index # for these columns: futureproofing, in case more cols are added later
+			tax_headers = ['effective date']
+			for i in range(len(tax_headers)):
+				try:
+					#index = header.index( tax_headers[i] )
+					#output[index] = line[index]
+					#messaging("Step %s, index of %s: %s" % ( i, tax_headers[i] , header.index( tax_headers[i] )) )
+					output[ header.index( tax_headers[i] ) ] = line[ header.index( tax_headers[i] ) ]
+				except ValueError:
+					if count==1:
+						messaging("Warning: %s column is missing." % tax_headers[i] )
+						if warning == 0:
+							messaging("Available columns: %s" % header)
+							warning = 1
+				except IndexError:
+					messaging("Error on line %s of file. Count: %s, Step: %s, Index of %s: %s" % ( linenum, count, i, tax_headers[i] , header.index( tax_headers[i] )) )
+					messaging("Person: %s\nProblematic line: %s" % (output,line) )
+					return("error")
 		
-		# # skip 2 lines b/c they add nothing
-		# temp = infile.readline() #line 4
-		# temp = infile.readline() #line 5
+		## skip 2 lines (subline==3 and 4) b/c they add nothing
 		
-		# # EEOC categories are on the sixth line
-		# line6 = infile.readline()
-		# line6 = line6.split(",")
-		# line6[-1] = line6[-1].strip()
-		# if len(header) != len(line2):
-			# messaging("Error: Different number of columns in record %s, line 6 than in header" % count)
-			# return("error")
-		# eeoc_headers = ["eeoc", "eeoc 2"]
-		# for i in range(2):
-			# try:
-				# line[ header.index( eeoc_headers[i] ) ] = line2[ header.index( eeoc_headers[i] ) ]
-			# except ValueError:
-				# if count==1:
-					# messaging("Warning: %s column is missing" % eeoc_headers[i] )
-					# if warning == 0:
-						# messaging("File Headers: %s" % header)
-						# warning = 1
-		
-		# #write our consolidated data to file!
-		# linestr = ','.join(map(str, (line[ i ] for i in range(0,len(header)) ))) + "\n"
-		# # http://stackoverflow.com/questions/44778/
-		# outfile.write( linestr )
+		## EEOC categories are on the sixth line
+		if subline==5:
+			if len(header) != len(line):
+				messaging("Error: Different number of columns in record %s, line 2 (file line # %s) than in header." % (count, linenum) )
+				messaging("Person: %s\nProblematic line: %s" % (line,line2) )
+				return("error")
+			##get the index # for these columns: futureproofing, in case more cols are added later
+			tax_headers = ['eeoc','eeoc 2']
+			for i in range(len(tax_headers)):
+				try:
+					#index = header.index( tax_headers[i] )
+					#output[index] = line[index]
+					#messaging("Step %s, index of %s: %s" % ( i, tax_headers[i] , header.index( tax_headers[i] )) )
+					output[ header.index( tax_headers[i] ) ] = line[ header.index( tax_headers[i] ) ]
+				except ValueError:
+					if count==1:
+						messaging("Warning: %s column is missing." % tax_headers[i] )
+						if warning == 0:
+							messaging("Available columns: %s" % header)
+							warning = 1
+				except IndexError:
+					messaging("Error on line %s of file. Count: %s, Step: %s, Index of %s: %s" % ( linenum, count, i, tax_headers[i] , header.index( tax_headers[i] )) )
+					messaging("Person: %s\nProblematic line: %s" % (output,line) )
+					return("error")
+	
+	##write our consolidated FOR THE LAST PERSON to file!
+	linestr = ','.join(map(str, (output[ i ] for i in range(len(header)) ))) + "\n"
+	# http://stackoverflow.com/questions/44778/
+	outfile.write( linestr )
 	
 	outfile.close()
 	f.close()
