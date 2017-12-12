b900f0eb047bcd31649bb369ea46873097e8273e
tshynik <lawolberg@gmail.com>
2017-12-12 13:17:46 -0500

    Most basic file opens and closes work.

--- ProcessDSExports.py
+++ ProcessDSExports.py
@@ -5,6 +5,7 @@
 
 import tkinter as tk
 from tkinter import filedialog as tkfile
+import csv
 
 ## TODO: It'd be nice if I could get a Windows Explorer to open, pointed at the directory where the file was created. This does not want to work, however.
 # https://stackoverflow.com/questions/36449361/tkinter-way-to-open-a-directory-window-in-windows-explorer
@@ -33,102 +34,131 @@ def messaging(msg_text):
 
 ## The function that actually does data processing.
 def formatfile(infilename, outfilename):
-	infile = open(infilename, "r")
-	header = infile.readline()
-	#messaging("Opened file %s" % infilename)
+	f = open(infilename, "r")
+	infile = csv.reader(f)
 	
+	## create outfile:
 	outfilename = outfilename + '.csv'
 	outfile = open(outfilename, "w")
 	
-	header = header.split(",")
-	if len(header)==1:
-		messaging("Error: First line (header) is not comma-separated. Check that the first line contains the headers. Also check that this is actually a CSV file.")
-		return("error")
-	header[-1] = header[-1].strip()
-	
-	newheader = ','.join(map(str, (header[ i ] for i in range(0,len(header))))) + "\n"
-	outfile.write( newheader )
-	
-	# clean up and write to outfile:
 	count = 0
 	warning = 0
+	linenum = 0
 	for line in infile:
-		line = line.split(",")
-		line[-1] = line[-1].strip()
+		linenum += 1
+		## process header line
+		if linenum == 1:
+			messaging("Header: %s" % line)
+	
+	# header = infile.readline()
+	# #messaging("Opened file %s" % infilename)
+	
+	
+	
+	# header = header.split(",")
+	# if len(header)==1:
+		# messaging("Error: First line (header) is not comma-separated. Check that the first line contains the headers. Also check that this is actually a CSV file.")
+		# return("error")
+	# header[-1] = header[-1].strip()
+	# #messaging("File Headers before: %s" % header)
+	# for i in range(len(header)):
+		# header[i] = header[i].strip('\"\'')
+		# #messaging( header[i] )
+	# #messaging("File Headers after: %s" % header)
+	
+	# newheader = ','.join(map(str, (header[ i ] for i in range(0,len(header))))) + "\n"
+	# outfile.write( newheader )
+	
+	# # clean up and write to outfile:
+	
+	# for line in infile:
+		# #line = line.split(",")
+		# #line[-1] = line[-1].strip()
+		# messaging("line: " % line)
 		
-		if( len(line[0])==0 ):
-			continue
-			#if this isn't a new person (the "office code" field is blank), go to next line.
+		# linenum += 1
 		
-		count += 1
+		# #if this isn't a new person (the "office code" field is blank), go to next line.
+		# line[0] = line[0].strip('\"\'')
+		# if( len(line[0])==0 ):
+			# continue
 		
-		# tax form fields are on the second line
-		line2 = infile.readline()
-		line2 = line2.split(",")
-		if len(header) != len(line2):
-			messaging("Error: Different number of columns in record %s, line 2 than in header." % count)
-			return("error")
-		line2[-1] = line2[-1].strip()
-		#get the index # for these columns: futureproofing, in case more cols are added later
-		tax_headers = ["w4", "witholding", "additional amount", "exempt"]
-		for i in range(4):
-			try:
-				#index = header.index( tax_headers[i] )
-				#line[index] = line2[index]
-				line[ header.index( tax_headers[i] ) ] = line2[ header.index( tax_headers[i] ) ]
-			except ValueError:
-				if count==1:
-					messaging("Warning: %s column is missing" % tax_headers[i] )
-					if warning == 0:
-						messaging("File Headers: %s" % header)
-						warning = 1
+		# count += 1
 		
-		# effective date is on the third line
-		line3 = infile.readline()
-		line3 = line3.split(",")
-		if len(header) != len(line2):
-			messaging("Error: Different number of columns in record %s, line 3 than in header" % count)
-			return("error")
-		line3[-1] = line3[-1].strip()
-		try:
-			effe_index = header.index("effective date")
-			line[effe_index] = line3[effe_index]
-		except ValueError:
-			if count==1:
-				messaging("Warning: effective date column is missing" )
-				if warning == 0:
-					messaging("File Headers: %s" % header)
-					warning = 1
+		# # tax form fields are on the second line
+		# line2 = infile.readline()
+		# line2 = line2.split(",")
+		# line2[-1] = line2[-1].strip('\"\'')
+		# if len(header) != len(line2):
+			# messaging("Error: Different number of columns in record %s, line 2 (file line # %s) than in header." % (count, linenum) )
+			# messaging("Person: %s\nProblematic line: %s" % (line,line2) )
+			# return("error")
+		# line2[-1] = line2[-1].strip()
+		# #get the index # for these columns: futureproofing, in case more cols are added later
+		# tax_headers = ['w4', 'witholding', 'additional amount', 'exempt']
+		# for i in range(len(tax_headers)):
+			# try:
+				# #index = header.index( tax_headers[i] )
+				# #line[index] = line2[index]
+				# #messaging("Step %s, index of %s: %s" % ( i, tax_headers[i] , header.index( tax_headers[i] )) )
+				# line[ header.index( tax_headers[i] ) ] = line2[ header.index( tax_headers[i] ) ]
+			# except ValueError:
+				# if count==1:
+					# messaging("Warning: %s column is missing" % tax_headers[i] )
+					# if warning == 0:
+						# messaging("File Headers: %s" % header)
+						# warning = 1
+			# except IndexError:
+				# messaging("Error on line %s of file. Count: %s, Step: %s, Index of %s: %s" % ( linenum, count, i, tax_headers[i] , header.index( tax_headers[i] )) )
+				# messaging("Person: %s\nProblematic line: %s" % (line,line2) )
+				# return("error")
 		
-		# skip 2 lines b/c they add nothing
-		temp = infile.readline() #line 4
-		temp = infile.readline() #line 5
+		# # effective date is on the third line
+		# line3 = infile.readline()
+		# line3 = line3.split(",")
+		# if len(header) != len(line2):
+			# messaging("Error: Different number of columns in record %s, line 3 than in header" % count)
+			# return("error")
+		# line3[-1] = line3[-1].strip()
+		# try:
+			# effe_index = header.index("effective date")
+			# line[effe_index] = line3[effe_index]
+		# except ValueError:
+			# if count==1:
+				# messaging("Warning: effective date column is missing" )
+				# if warning == 0:
+					# messaging("File Headers: %s" % header)
+					# warning = 1
 		
-		# EEOC categories are on the sixth line
-		line6 = infile.readline()
-		line6 = line6.split(",")
-		line6[-1] = line6[-1].strip()
-		if len(header) != len(line2):
-			messaging("Error: Different number of columns in record %s, line 6 than in header" % count)
-			return("error")
-		eeoc_headers = ["eeoc", "eeoc 2"]
-		for i in range(2):
-			try:
-				line[ header.index( eeoc_headers[i] ) ] = line2[ header.index( eeoc_headers[i] ) ]
-			except ValueError:
-				if count==1:
-					messaging("Warning: %s column is missing" % eeoc_headers[i] )
-					if warning == 0:
-						messaging("File Headers: %s" % header)
-						warning = 1
+		# # skip 2 lines b/c they add nothing
+		# temp = infile.readline() #line 4
+		# temp = infile.readline() #line 5
 		
-		#write our consolidated data to file!
-		linestr = ','.join(map(str, (line[ i ] for i in range(0,len(header)) ))) + "\n"
-		# http://stackoverflow.com/questions/44778/
-		outfile.write( linestr )
+		# # EEOC categories are on the sixth line
+		# line6 = infile.readline()
+		# line6 = line6.split(",")
+		# line6[-1] = line6[-1].strip()
+		# if len(header) != len(line2):
+			# messaging("Error: Different number of columns in record %s, line 6 than in header" % count)
+			# return("error")
+		# eeoc_headers = ["eeoc", "eeoc 2"]
+		# for i in range(2):
+			# try:
+				# line[ header.index( eeoc_headers[i] ) ] = line2[ header.index( eeoc_headers[i] ) ]
+			# except ValueError:
+				# if count==1:
+					# messaging("Warning: %s column is missing" % eeoc_headers[i] )
+					# if warning == 0:
+						# messaging("File Headers: %s" % header)
+						# warning = 1
+		
+		# #write our consolidated data to file!
+		# linestr = ','.join(map(str, (line[ i ] for i in range(0,len(header)) ))) + "\n"
+		# # http://stackoverflow.com/questions/44778/
+		# outfile.write( linestr )
 	
 	outfile.close()
-	infile.close()
+	f.close()
 	
 	messaging( "Finished %s! %s records processed." % (outfilename, count) )
 	return("ok")
